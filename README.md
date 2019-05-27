5d5d84efca8de6f596318b2a95f406e2accd1217
Josiee <josieexception@outlook.com>
2019-05-28 00:09:14 +0800

    add op to save patameters

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CNNs_for_classifing_CIFAR10
 
- Implement of LeNet, AlexNet, VGG16, GoogLeNet, ResNet50 for classifing on CIFAR10 with tensorflow. In order to adapt to the size of CIFAR10, I adjusted some parameters in the network. And it's easy to fit it taining on other dataset.  
+Implement of CNNs for classifing on CIFAR10 with tensorflow(CPU). In order to adapt to the size of CIFAR10, I adjusted some parameters in the network. And it's easy to fit it taining on other dataset. 
 
 - LeNet
 - AlexNet
@@ -10,19 +10,40 @@
 
 ## Requirements
 
-- python 3.6.5
-- tensorflow 1.2.1
-- numpy
+- python 3.6.3
+- tensorflow 1.13.1
+- numpy 1.16.3
 - CIFAR10 can be download [here][1]. The path to ‘cifar-10-batches-py’ can be specified with the optional parameter ‘--dataset_dir’, which by default is placed in the root directory.
-	
-## Train
+
+## Train and Test
+
+Here I only iterate 20 epoches (10000 steps), you can increase the number of iterations by using the last trained model to achieve higher accuracy. Besides, you can also change `learning rate` and `steps` in `main.py`.
 
 ```
-# Train by default.
+# Train and test by default.
 $ python main.py
 
-# Train with optional patameters.
-$ python main.py --model_type [LeNet/AlexNet/VGG16/GoogLeNet/ResNet50] --dataset_dir [Path to cifar-10-batches-py]
+# Train with optional patameters and test.
+$ python main.py	--model_type	[LeNet/AlexNet/VGG16/GoogLeNet/ResNet50] 
+					--dataset_dir	[Path to cifar-10-batches-py] 
+					--model_dir		[A .ckpt file of pretrained model or A folder for saving model] 
+```
+
+## Use GPU
+
+- CUDA 8.0.61
+- CUDNN 5.1
+- tensorflow_gpu 1.2.0
+
+```
+# Chose GPU to use
+$ CUDA_VISIBLE_DEVICES=0 python main.py (optional patameters...)
+```
+
+## Logs
+
+```
+$ tensorboard --logdir=/logs
 ```
 
   [1]: https://www.cs.toronto.edu/~kriz/cifar.html
--- main.py
+++ main.py
@@ -13,18 +13,35 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("--model_type", dest='model_type', default='LeNet', help='type of model')
 parser.add_argument('--dataset_dir', dest='dataset_dir', default='cifar-10-batches-py/', help='path of the dataset')
+parser.add_argument('--model_dir', dest='model_dir', default='model/', help='path to saving/restoring model')
 
 args = parser.parse_args()
 
+def unpickle(file):
+
+	with open(file, 'rb') as fo:
+		dict = pickle.load(fo, encoding='bytes')
+	return dict
+
 def main():
 
-	# Get data.
+	# Path to loading model.
+	if args.model_dir[-5:] != '.ckpt':
+		MODEL_DIR = args.model_dir + args.model_type + '.ckpt'
+		PRE_TRAINED = False
+	else:
+		# Train from pretrained model.
+		MODEL_DIR = args.model_dir
+		PRE_TRAINED = True
+		print('Training based on {}'.format(MODEL_DIR))
+
+	# Load data.
 	CIFAR_DIR = args.dataset_dir
 	dirs = ['batches.meta','data_batch_1','data_batch_2','data_batch_3','data_batch_4','data_batch_5','test_batch']
 	all_data = [0,1,2,3,4,5,6]
-
-	for i,direc in zip(all_data,dirs):
-		all_data[i] = pickle.load(open(CIFAR_DIR+direc, 'rb'))
+	for i,direc in zip(all_data, dirs):
+		all_data[i] = unpickle(CIFAR_DIR + direc)
+	print("CIFAR10 has loaded!")
 
 	x = tf.placeholder(tf.float32, shape = [None, 32, 32, 3])
 	y_true = tf.placeholder(tf.float32, shape = [None, 10])
@@ -41,12 +58,13 @@ def main():
 	elif args.model_type == 'ResNet50':
 		model = ResNet50(x = x, keep_prob = keep_prob, num_classes = 10)
 	else:
-		print('Please check the model_type!')
+		print("'{}' is not recognized. "
+			"Use 'LeNet' / 'AlexNet' / 'VGG16' / 'GoogLeNet' / 'ResNet50'. ".format(args.command))
 
-	score = model.output
+	y = model.output
 
 	with tf.name_scope('cross_entropy'): 
-		cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y_true, logits = score))
+		cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y_true, logits = y))
 	tf.summary.scalar('cross_entropy', cross_entropy)
 
 	with tf.name_scope('train'):
@@ -54,28 +72,41 @@ def main():
 		train = optimizer.minimize(cross_entropy)
 
 	with tf.name_scope('accuracy'):
-		matches = tf.equal(tf.argmax(score, 1), tf.argmax(y_true, 1))
+		matches = tf.equal(tf.argmax(y, 1), tf.argmax(y_true, 1))
 		acc = tf.reduce_mean(tf.cast(matches, tf.float32))
 	tf.summary.scalar('accuracy', acc)
 
+	# Add an op to initialize the variables.
 	init = tf.global_variables_initializer()
+
+	# Add ops to save and restore all the variables.
+	saver = tf.train.Saver()
 
 	# steps = 10,000 will create 20 epochs.
 	# There are a total of 50,000 images in the training set.
 	# (10,000 * 100) / 50,000 = 20
-	steps = 10001
+	steps = 10000
 
 	ch = CifarHelper(all_data)
 	# pre-processes the data.
 	ch.set_up_images()
 
 	with tf.Session() as sess:
+
+		if PRE_TRAINED:
+			# Restore variables from disk.
+			saver.restore(sess, MODEL_DIR)
+			print("Model restored.")
+		else:
+			# Initialize the variables.
+			sess.run(init)
+
 		# Write Logs
-		sess.run(init)
-		merged_summary_op = tf.contrib.deprecated.merge_all_summaries()
+		merged_summary_op = tf.summary.merge_all()
 		summary_writer = tf.summary.FileWriter('logs/', sess.graph)
+
 		for i in range(steps):
-			# get next batch of data.
+			# Get next batch of data.
 			batch = ch.next_batch(100)
 			# On training set.
 			sess.run(train, feed_dict = {x : batch[0], y_true : batch[1], keep_prob : 0.5})
@@ -87,6 +118,10 @@ def main():
 				# On valid/test set.
 				print(sess.run(acc, feed_dict = {x : ch.test_images, y_true : ch.test_labels, keep_prob : 1.0}))
 				summary_writer.add_summary(sess.run(merged_summary_op, feed_dict = {x : ch.test_images, y_true : ch.test_labels, keep_prob : 1.0}), i)
+		
+		# Save the variables to disk.
+		save_path = saver.save(sess, MODEL_DIR)
+		print("Model saved in path: %s" % save_path)
 
 		summary_writer.close()
 
